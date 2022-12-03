9ec8dc133ef16e80f8474ab0093005d56686b171
Sheol_ <116242960+Sheol0105@users.noreply.github.com>
2022-12-03 22:27:00 +0800

    Update text.c

--- text.c
+++ text.c
@@ -1,12 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+//打印99乘法表
 int main()
 {
-	int i = 0;
-	for (i = 1; i <= 9; i++)
+	int i = 0;https://github.com/Sheol0105/Green/blob/main/text.c
+	for (i = 1; i <= 9; i++) //行数
 	{
 		int j = 0;
-		for (j = 1; j <= i; j++)
+		for (j = 1; j <= i; j++) //打印一行
 			printf("%4d * %d = %-2d", i, j, i * j);
 		printf("\n");
 	}
